41f82cf2fddffb171eaa90313885ccbe2b44de6f
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-04-20 15:08:43 -0300

    feat: Created Middlewares of JSON

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,4 +1,5 @@
 import http from 'node:http';
+import { json } from './middlewares/json.js';
 
 // - Criar usuários
 // - Listagem de usuários
@@ -34,22 +35,10 @@ const users = [];
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
-  const buffers = [];
-
-  for await (const chunck of req) {
-    buffers.push(chunck);
-  }
-
-  try {
-    req.body = JSON.parse(Buffer.concat(buffers).toString());
-  } catch {
-    req.body = null;
-  }
+  await json(req, res);
 
   if (method === 'GET' && url === '/users') {
-    return res
-      .setHeader('Content-type', 'application/json')
-      .end(JSON.stringify(users));
+    return res.end(JSON.stringify(users));
   }
 
   if (method === 'POST' && url === '/users') {
